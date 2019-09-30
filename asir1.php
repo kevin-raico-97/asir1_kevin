b7852ffafce2ab34774508ab783efd1cb3703b7a
kevin-raico-97 <kevinasir@outlook.com>
2019-09-30 10:52:46 +0200

    bien

--- asir1.php
+++ asir1.php
@@ -3,7 +3,10 @@
 <a href="http://192.168.0.82/asir1_saioa/asir1.php">saioa</a>
 <a href="http://192.168.0.92/asir176/asir1.php">alejandro</a>
 <?php
+
 $f=fopen('visitas.txt','a');
-fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,date(' Y-m-d H:i'));
+fwrite($f,"\r\n");
 fclose($f);
 ?>
