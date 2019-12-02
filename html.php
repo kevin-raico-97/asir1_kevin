f700bd6ed85fc04432fe3a5afd14e27e99fa14fc
kevin-raico-97 <kevinasir@outlook.com>
2019-12-02 13:01:25 +0100

    html

--- html.php
+++ html.php
@@ -4,17 +4,78 @@
 </head>
 <body bgcolor="#BEABD7">
 <Center>
-<table width="85%"  height="85%">
+<table width="75%"  height="75%">
 <h1 align="center">Francia</h1>
-<h2> París </h2>
-<h4>París es la capital de Francia y constituye una de las ciudades más
+<h2><u><b> París </b></u></h2>
+<br>
+<font color="#036E8A" size="2px" face="Comic Sans MS">París es la capital de Francia y constituye una de las ciudades más
  importantes e influyentes del mundo. Desde el punto de vista turístico,
  París es una de las ciudades más visitadas de Europa. Con nuestra guía
  de París podrás descubrir todos los secretos de la ciudad además de
- conseguir los mejores consejos para viajar a París ahorrando dinero.</h4>
+ conseguir los mejores consejos para viajar a París ahorrando dinero.</font>
+<br><br><br>
+
+<img src="https://i2.wp.com/unaideaunviaje.com/wp-content/uploads/2019/07/Que-ver-en-paris-unaideaunviaje-2.jpg?resize=999%2C600&ssl=1" width="750">
 
 
+<br><br><br>
+<h1>¿Por qué visitar París?</h1>
+<p>París es una ciudad que parece diseñada para el disfrute del viajero. 
+Sus calles, plazas, edificios, jardines y monumentos parecen ideados para
+ que cualquier persona que visite París desee volver.<br><br>
+ 
+ Visitar la Torre Eiffel, el Arco del Triunfo o la Catedral de Notre Dame, 
+ acudir a un espectáculo de cabaret en el mítico Moulin Rouge, o bien pasear
+ por algunos de los barrios más pintorescos de la ciudad como Montmartre o 
+ Montparnasse, son algunas de las experiencias que todo el mundo debería 
+ tener la ocasión de disfrutar.<br><br>
+ 
+ Cualquier excusa es buena para viajar a París, tanto su cercanía con España,
+ como el precio de los vuelos de bajo coste. Y si tenéis niños, 
+ ¿por qué no regalarles un viaje a Disneyland?</p>
+ <br><br><br>
+ 
+<h1>¿Cómo organizar el viaje?</h1>
+<p>Antes de viajar te sugerimos que comiences informándote sobre la historia
+ de París y algunos datos prácticos que pueden ser de utilidad.<br><br>
+ 
+ Tras conocer los lugares más interesantes de París, los principales museos,
+ las mejores zonas para alojarse y los platos más típicos, sólo falta 
+ hacer las maletas y prepararse para disfrutar de una de las ciudades 
+ más románticas del mundo.</p>
+ 
+<table border="2" bordercolor="red"">
+  <tr align="center">
+    <td bgcolor="red">Temperatura</td>
+    <td bgcolor="red">Ciudad</td>
+  </tr>
+<tr align="center">
+	<td>París</td>
+	<td>7º</td>
+</tr>
+<tr align="center">
+	<td>Marsella</td>
+	<td>13º</td>
+</tr>
+<tr align="center">
+	<td>Lyon</td>
+	<td>7º</td>
+</tr>
+<tr align="center">
+	<td>Toulouse</td>
+	<td>10º</td>
+</tr>
+<tr align="center">
+	<td>Nantes</td>
+	<td>8º</td>
+</tr>
+<tr align="center">
+	<td>Burdeos</td>
+	<td>8º</td>
+</tr>
+</table>
 
-
+</center>
+</table>
 </body>
 </html>
