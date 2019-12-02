71b200015513ff98889ffa874754121b121bb64f
kevin-raico-97 <kevinasir@outlook.com>
2019-12-02 10:33:09 +0100

    laboral d

--- triangulos_a.php
+++ triangulos_a.php
@@ -8,13 +8,13 @@ function tipo_lados($a,$b,$c){
 	if($a==$b and $b==$c)
 		$r="equilatero";
 
-	echo "El triangulo es: ";
-	echo $r;
+	$r=return
 	echo '<br>';
 }
 
 echo 'Pruebas sobre triángulos<br>';
-tipo_lados(1,1,1);	
+echo "El triangulo es: ";
+echo tipo_lados(5,1,1);	
 
 tipo_lados(1,1,2);
 ?>
